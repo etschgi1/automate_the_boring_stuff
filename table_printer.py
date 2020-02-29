7a89f1c4faf84b6be6744b3b09ddf43c8b0cef43
Elias Wachmann <elias.wachmann@gmail.com>
2020-02-29 16:29:15 +0100

    initial commit. not quite working

---
+++ table_printer.py
@@ -0,0 +1,14 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+def print_table(table):
+    colum_widths = [0]*len(table)
+    for tablelist in range(len(colum_widths)):
+        table[tablelist]
+        pass
+    print(colum_widths)
+
+
+print_table(tableData)
