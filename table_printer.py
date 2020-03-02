c38669af2c59114b0ba4e4c982a2a79ccc3ba4c0
Elias Wachmann <elias.wachmann@gmail.com>
2020-03-02 15:56:23 +0100

    Even better version with seperation charakters

--- table_printer.py
+++ table_printer.py
@@ -2,10 +2,15 @@ tableData = [['oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
+tableData2 = [['oranges', 'cherries', 'banana', 'Alice', 'Bob', 'Carol',
+               'David'],
+              ['Alice', 'Bob', 'Carol', 'David'],
+              ['dogs', 'cats', 'moose', 'goose']]
+
 
 def print_table(data):
     table_length = []  # Stores individual table length
-    output = []
+    output = []  # Stores Output
     for table in data:
         text = table_to_string(table)
         table_length.append(len(text))
@@ -13,14 +18,17 @@ def print_table(data):
     table_size = max(table_length)
     for text in output:
         print(text.rjust(table_size))
+        if text == output[-1]:
+            print("\n".rjust(table_size, "-"))
 
 
 def table_to_string(tablenum):
     string = ""
     for word in tablenum:
         word = word + " "
-        string += word.title()
+        string += word.title()  # adds words in table together
     return string
 
 
 print_table(tableData)
+print_table(tableData2)
