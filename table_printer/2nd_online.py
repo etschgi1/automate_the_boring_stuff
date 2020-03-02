d2ee2cdd62e195f02e96e746ac8954ef7c2d420a
Elias Wachmann <elias.wachmann@gmail.com>
2020-03-02 16:42:21 +0100

    .

---
+++ table_printer/2nd_online.py
@@ -0,0 +1,7 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+for a, b, c in zip(tableData[0], tableData[1], tableData[2]):
+    print(a.rjust(10), b.rjust(10), c.rjust(10))
