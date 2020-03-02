d4e614cc73b764618f15467848ab9f6d67fc3c84
Elias Wachmann <elias.wachmann@gmail.com>
2020-03-02 15:45:41 +0100

    working table plotter

--- table_printer.py
+++ table_printer.py
@@ -1,14 +1,26 @@
-tableData = [['apples', 'oranges', 'cherries', 'banana'],
+tableData = [['oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 
-def print_table(table):
-    colum_widths = [0]*len(table)
-    for tablelist in range(len(colum_widths)):
-        table[tablelist]
-        pass
-    print(colum_widths)
+def print_table(data):
+    table_length = []  # Stores individual table length
+    output = []
+    for table in data:
+        text = table_to_string(table)
+        table_length.append(len(text))
+        output.append(text)
+    table_size = max(table_length)
+    for text in output:
+        print(text.rjust(table_size))
+
+
+def table_to_string(tablenum):
+    string = ""
+    for word in tablenum:
+        word = word + " "
+        string += word.title()
+    return string
 
 
 print_table(tableData)
